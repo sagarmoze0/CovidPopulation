b3f12e4f3fc3ce48f929a24996f885328d923c18
sagarmoze0 <sagarmoze0@gmail.com>
2024-04-23 18:37:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import HistoricalData from './components/HistoricalData';
 function App() {
   return (
-    <div className="App">
+    <div className="p-4 sm:p-8 md:p-12 lg:p-16 xl:p-20">
       <HistoricalData />
     </div>
   );
